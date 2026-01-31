9a1347fb1e2546a582e444f522f5137724c98af2
Hanh246 <149309654+Hanh246@users.noreply.github.com>
2026-01-31 11:29:54 +0700

    add validate ad /api

--- src/main/java/com/exe201/tutorlink/main/controller/UserController.java
+++ src/main/java/com/exe201/tutorlink/main/controller/UserController.java
@@ -5,6 +5,7 @@ import com.exe201.tutorlink.main.dto.LoginDTO;
 import com.exe201.tutorlink.main.dto.UserBaseDTO;
 import com.exe201.tutorlink.main.dto.UserDTO;
 import com.exe201.tutorlink.main.service.UserService;
+import com.exe201.tutorlink.main.validator.UserValidator;
 import jakarta.validation.Valid;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,14 +13,28 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/user")
 public class UserController{
     @Autowired
     private UserService userService;
+    @Autowired
+    private UserValidator validation;
 
     @PostMapping
     public ResponseEntity<BaseResponse<UserDTO>> create(@Valid @RequestBody UserDTO dto) {
+        Map<String,String> validator = validation.validateCreate(dto);
+        if(validator != null && !validator.isEmpty()){
+            Map<String,String> errors = UserValidator.convertKeys(validator);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(BaseResponse.<UserDTO>builder()
+                    .success(false)
+                    .data(null)
+                    .message("Đã có lỗi xảy ra")
+                    .errors(errors)
+                    .build());
+        }
         UserDTO created = userService.save(dto);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(BaseResponse.<UserDTO>builder().success(true).data(created).build());
